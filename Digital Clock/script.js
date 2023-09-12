7fe4f8e8125c7ebcaba624dc157102727300d28e
Harsh Chauhan <109052056+HK24C@users.noreply.github.com>
2023-09-12 22:00:31 +0530

    Add files via upload

---
+++ Digital Clock/script.js
@@ -0,0 +1,26 @@
+let hrs = document.getElementById("hrs");
+let min = document.getElementById("min");
+let sec = document.getElementById("sec");
+let ampm = document.getElementById("ampm");
+
+function updateClock() {
+    setInterval(() => {
+    let currentTime = new Date();
+    let hours = currentTime.getHours();
+    let minutes = currentTime.getMinutes();
+    let seconds = currentTime.getSeconds();
+    let period = hours >= 12 ? "PM" : "AM"; 
+
+    
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+
+    
+    hrs.innerHTML = (hours < 10 ? "0" : "") + hours;
+    min.innerHTML = (minutes < 10 ? "0" : "") + minutes;
+    sec.innerHTML = (seconds < 10 ? "0" : "") + seconds;
+    ampm.innerHTML = period;
+    },1000)
+}
+
+updateClock(); 
