eb7fbe10dac82354e56b75ccb048e510a42204f7
Harsh Chauhan <109052056+HK24C@users.noreply.github.com>
2023-09-18 15:46:59 +0530

    Add files via upload

---
+++ StopWatch/script.js
@@ -0,0 +1,39 @@
+let [seconds, minutes, hours] = [0,0,0];
+let disptime = document.getElementById("disptime");
+let timer = null;
+
+
+function stopwatch(){
+    seconds++;
+    if(seconds==60)
+    {
+        seconds=0;
+        minutes++;
+        if(minutes==60)
+        {
+            minutes = 0;
+            hours++;
+        }
+    }
+let h = hours < 10?"0" +hours:hours;
+let m = minutes < 10?"0" +minutes:minutes;
+let s = seconds < 10?"0" +seconds:seconds;
+
+    displayTime.innerHTML = hours +":"+minutes +":"+seconds; 
+}
+
+function watchStart(){
+    if(timer!=null){
+        clearInterval(timer);
+    }
+    timer = setInterval(stopwatch,1000);
+}
+
+function watchStop(){
+    clearInterval(timer);
+}
+function watchReSet(){
+    clearInterval(timer);
+    [seconds, minutes, hours] = [0,0,0];
+    displayTime.innerHTML = "00:00:00";
+}
